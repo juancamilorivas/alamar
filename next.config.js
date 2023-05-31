28e2cb8a79c5c03cd40d8be1a32e48e70b62fadd
juancamilorivas <bogota.diamond@hotmail.com>
2023-05-30 22:17:34 -0500

    next conig working now

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-  
-  module.exports = {
-    output: "export",
-  };
-  
-  
+  const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+    }
+    module.exports = nextConfig
