5a1663e4df1babce1eea26287ff153e1ace693a9
juancamilorivas <bogota.diamond@hotmail.com>
2023-05-31 16:04:53 -0500

    waiting

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 
 
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    images: {
-      unoptimized: true
-  }
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true } 
   }
 
-    
-    module.exports = nextConfig
+  
+  module.exports = nextConfig
+
+
+
+
