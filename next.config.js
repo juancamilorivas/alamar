7a027ed21faccb5af7da23fd347c65cb1d6852cb
juancamilorivas <bogota.diamond@hotmail.com>
2023-05-31 15:51:38 -0500

    test online

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 
 
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    output: 'export',
-    images: { unoptimized: true } 
-    }
+  const nextConfig = {}
 
     
     module.exports = nextConfig
