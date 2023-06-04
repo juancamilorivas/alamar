a88c9ae8ab0562cd14d402cd44157ebe7c5abb0f
juancamilorivas <bogota.diamond@hotmail.com>
2023-06-04 16:55:56 -0500

    gitignore updated

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMpeKcxsOcqBt6gN84u0QhfjXupqWBo6g",
-  authDomain: "alamar3-93b2d.firebaseapp.com",
-  projectId: "alamar3-93b2d",
-  storageBucket: "alamar3-93b2d.appspot.com",
-  messagingSenderId: "864999022628",
-  appId: "1:864999022628:web:2ae48b67e407ff5072b8b1",
-  measurementId: "G-3NZJZYYT45"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
