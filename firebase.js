8f0c08de6230e71a1b03ae828f9ae04339a654df
juancamilorivas <bogota.diamond@hotmail.com>
2023-06-05 16:50:39 -0500

    test

--- firebase.js
+++ firebase.js
@@ -12,6 +12,16 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
+// Debugging: Print the values of the environment variables
+console.log('API Key:', process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log('Auth Domain:', process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log('Project ID:', process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log('Storage Bucket:', process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+console.log('Messaging Sender ID:', process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+console.log('App ID:', process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+console.log('Measurement ID:', process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID);
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
