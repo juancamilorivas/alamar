fcae7bec137d68492dfa281f09139fa5721bb66b
juancamilorivas <bogota.diamond@hotmail.com>
2023-06-11 15:28:23 -0500

    process instead secrets

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: secrets.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: secrets.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: secrets.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: secrets.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: secrets.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: secrets.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: secrets.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 
 // Initialize Firebase
