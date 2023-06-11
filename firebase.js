fb501c1e8dc7ff1bd694cc9fcf3197e0b3048ad3
juancamilorivas <bogota.diamond@hotmail.com>
2023-06-11 17:31:13 -0500

    testing debug

--- firebase.js
+++ firebase.js
@@ -13,13 +13,13 @@ const firebaseConfig = {
 };
 
 console.log("hello world")
-console.log("apiKey:", firebaseConfig.apiKey);
-console.log("authDomain:", firebaseConfig.authDomain);
-console.log("projectId:", firebaseConfig.projectId);
-console.log("storageBucket:", firebaseConfig.storageBucket);
-console.log("messagingSenderId:", firebaseConfig.messagingSenderId);
-console.log("appId:", firebaseConfig.appId);
-console.log("measurementId:", firebaseConfig.measurementId);
+console.log("apiKey:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+console.log("authDomain:", process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN);
+console.log("projectId:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
+console.log("storageBucket:", process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET);
+console.log("messagingSenderId:", process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID);
+console.log("appId:", process.env.NEXT_PUBLIC_FIREBASE_APP_ID);
+console.log("measurementId:", process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
